111ed41707a257b751c5eadfc9a80ad3a9c273e9
hjunor <hjunor@gmail.com>
2020-05-12 11:08:02 -0300

    canvas box

---
+++ src/components/canvas/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../form/index'
+import './styles.css';
+
+
+export default function Canvas() {
+    return (
+        <div className='container'>
+            <div className="box"> 
+             <Form></Form>
+            </div>
+        </div>
+    )
+}
