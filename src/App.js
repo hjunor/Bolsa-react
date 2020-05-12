51108f9ae556e30308c83f591b9f6a21c1a1d270
hjunor <hjunor@gmail.com>
2020-05-12 11:05:11 -0300

    body aplication

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Canvas from './components/canvas/index';
+
+
+function App() {
+  return (
+    <div className="App">
+       <Canvas className="canvas"/>
+    </div>
+  );
+}
+
+export default App;
